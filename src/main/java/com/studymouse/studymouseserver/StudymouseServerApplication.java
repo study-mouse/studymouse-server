10b29564b69ceebe92ad53344e5010b4eb90d41f
mjung1798 <mor2222@naver.com>
2020-07-13 21:59:39 +0900

    Feat: project setting

---
+++ src/main/java/com/studymouse/studymouseserver/StudymouseServerApplication.java
@@ -0,0 +1,13 @@
+package com.studymouse.studymouseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudymouseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudymouseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studymouse/studymouseserver/StudymouseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studymouse.studymouseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymouseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
