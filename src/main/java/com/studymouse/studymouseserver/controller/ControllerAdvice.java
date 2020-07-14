f4138bcc9649d712336e6acae890bde6a16b9903
mjung1798 <mor2222@naver.com>
2020-07-15 00:01:09 +0900

    Feat: CRUD api
    CRUD API 작성 : U는 BM 상 추가 X
    R : time에 따라, paging에 따라 조회

--- src/main/java/com/studymouse/studymouseserver/controller/ControllerAdvice.java
+++ src/main/java/com/studymouse/studymouseserver/controller/ControllerAdvice.java
@@ -4,8 +4,15 @@ package com.studymouse.studymouseserver.controller;
  * Created by jyami on 2020/07/14
  */
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> invalidIdException(Exception e){
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
 }
