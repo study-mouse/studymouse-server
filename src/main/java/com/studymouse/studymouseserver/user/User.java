8a8f64a0194a55535175616aaa91a75ebdd16597
Gobukgol <zxczc159@gmail.com>
2020-07-14 16:44:55 +0900

    feat(User): 임시 User Entity 구현

---
+++ src/main/java/com/studymouse/studymouseserver/user/User.java
@@ -0,0 +1,48 @@
+package com.studymouse.studymouseserver.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(Long id, String name, String email, String picture) {
+        this.id =id;
+        this.name =name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
