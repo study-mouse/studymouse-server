8b14a17ee469b1834e7e599c0d0260263c3f5df7
Gobukgol <zxczc159@gmail.com>
2020-07-15 15:18:06 +0900

    feat(SecurityConfig): Spring Security configuration 클래스 구현
    현재 "/api/word/" 관련한 API 호출의 경우 권한이 사용자일 경우에만 허용, 나머지는 권한 상관없이 허용
    로그아웃 성공시 임시로 "/" url로 돌아가도록 설정

---
+++ src/main/java/com/studymouse/studymouseserver/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.studymouse.studymouseserver.security;
+
+import com.studymouse.studymouseserver.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig {
+
+    @RequiredArgsConstructor
+    @EnableGlobalMethodSecurity(securedEnabled = true)
+    static public class SecurityMajorConfig extends WebSecurityConfigurerAdapter {
+
+        private final CustomOAuthUserService customOAuthUserService;
+
+        @Override
+        protected void configure(final HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .headers().frameOptions().disable()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/api/word/**").hasRole(Role.USER.name())
+                    .anyRequest().permitAll()
+                    .and()
+                    .logout().logoutSuccessUrl("/")
+                    .and()
+                    .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(customOAuthUserService);
+        }
+    }
+}
