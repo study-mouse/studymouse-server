cd5beadad0ea8372769430d6591c5fc2ba83e704
Gobukgol <zxczc159@gmail.com>
2020-07-15 15:07:20 +0900

    feat(OAuthAttributes): OAuth 로그인시 반환하는 정보를 전달하는 Dto 클래스 구현

---
+++ src/main/java/com/studymouse/studymouseserver/security/dto/OAuthAttributes.java
@@ -0,0 +1,51 @@
+package com.studymouse.studymouseserver.security.dto;
+
+import com.studymouse.studymouseserver.user.Role;
+import com.studymouse.studymouseserver.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(final Map<String, Object> attributes, final String nameAttributeKey, final String name, final String email, final String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+
+    public static OAuthAttributes of(final String registrationId, final String userNameAttributeName, final Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(final String userNameAttributeName, final Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.USER)
+                .build();
+    }
+}
