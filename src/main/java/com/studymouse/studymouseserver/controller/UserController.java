42758f1ebe630ffce686e2e760b4bcd526949a2e
Gobukgol <zxczc159@gmail.com>
2020-07-17 21:54:15 +0900

    refactor(UserController): 자체 회원가입 기능 제거로인한 api 삭제

--- src/main/java/com/studymouse/studymouseserver/controller/UserController.java
+++ src/main/java/com/studymouse/studymouseserver/controller/UserController.java
@@ -1,48 +1,32 @@
 package com.studymouse.studymouseserver.controller;
 
-import com.studymouse.studymouseserver.security.CustomOAuthUserService;
 import com.studymouse.studymouseserver.security.dto.AccessUser;
-import com.studymouse.studymouseserver.service.UserService;
 import com.studymouse.studymouseserver.user.Type;
-import com.studymouse.studymouseserver.user.dto.UserLoginReqDto;
-import com.studymouse.studymouseserver.user.dto.UserReqDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("api/user")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
     private final HttpSession httpSession;
 
-    @PostMapping("")
-    public ResponseEntity<Void> saveUser(@RequestBody final UserReqDto userReqDto) {
-        userService.save(userReqDto);
-
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("login")
-    public ResponseEntity<Void> login(@RequestBody final UserLoginReqDto userLoginReqDto) {
-        AccessUser accessUser = userService.login(userLoginReqDto);
-
-        httpSession.setAttribute("user", accessUser);
-
-        return ResponseEntity.ok().build();
+    @PostMapping("login/google")
+    public String loginGoogle() {
+        return "redirect:/oauth2/authorization/google";
     }
 
     @GetMapping("logout")
-    public RedirectView logout() {
+    public @ResponseBody
+    RedirectView logout() {
         AccessUser accessUser = (AccessUser) httpSession.getAttribute("user");
 
         if (Objects.isNull(accessUser)) {
